ce4f6893d45d1e5cf163c68a696ff440f43c7806
Nayem <redofyt@gmail.com>
2026-03-12 09:25:00 +0600

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,24 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: '',
+			logo: {
+    			light: './src/assets/light-logo.svg',
+    			dark: './src/assets/dark-logo.svg',
+  			},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/nfsnayem/community' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Plugins',
+					autogenerate: { directory: 'plugins' },
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Themes',
+					autogenerate: { directory: 'themes' },
+				},
+				{
+					label: 'Template Kits',
+					autogenerate: { directory: 'templates' },
 				},
 			],
 		}),
